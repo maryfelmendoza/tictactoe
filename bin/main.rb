048afcdb27a3fc795d45334ed1b186b0dafb6074
yirano <tiffany.arellano@outlook.com>
2019-10-07 12:10:39 -0400

    Confirm that player inputs correctly
    User has to input something between 1 and 9. This will ensure that they do and if not, the program outputs an error message

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
+# Set up game
 puts "Welcome!"
 puts "Player 1, What is your name?"
 @player1 = gets.chomp
@@ -5,10 +6,30 @@ puts "Now, Player 2, What is your name?"
 @player2 = gets.chomp
 puts "Let's play! #{@player1} #{@player2}"
 count = 0
+
+# While nobody has won
 while count < 3
-puts "Choose a number between 1-9 #{@player1}"
-@player1input = gets.chomp
-puts "#{@player2} now its your turn, choose a number between 1-9"
-@player2 = gets.chomp
-count +=1
+
+  # get input from player 1
+  puts "Choose a number between 1-9 #{@player1}"
+  @player1_input = gets.chomp.to_i
+  
+  # Make sure user has the correct input
+  if @player1_input.is_a?(Integer) && @player1_input > 0 && @player1_input <= 9
+    puts "#{@player2} now its your turn, choose a number between 1-9"
+    @player2_input = gets.chomp
+    if @player2_input.is_a?(Integer) && @player2_input > 0 && @player2_input <= 9
+      puts "#{@player1} now its your turn, choose a number between 1-9"
+    else
+      # If player 2 has wrong input
+      puts "FOLLOW THE RULES"
+    end
+  else
+      # If player 1 has wrong input
+    puts "YOU GOTTA PUT IN SOMETHING RIGHT"
+  end
+  count +=1
 end
+
+# Congratulate winner
+puts "YOU WON #{@player1}!!!"
