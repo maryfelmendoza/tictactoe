4cb27174396261a011f7b3953b45a0c01d6fb6bf
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-07 12:46:27 -0500

    player 2 and switch

--- lib/game.rb
+++ lib/game.rb
@@ -21,12 +21,23 @@ class Game
   def play 
     puts self.board
     # While nobody has won
+      @current_player = 1
+      if @current_player == 1
       puts "Choose a number between 1-9 #{@player1}"
       @player1_input = gets.chomp.to_i
       @player1_played.push(@player1_input)
 
       @arr[@player1_input - 1] = " [ X ] "
       puts @arr.each_slice(3) { |x| puts x.join puts}
+      @current_player=2
+      else @current_player == 2
+      puts "Choose a number between 1-9 #{@player2}"
+      @player2_input = gets.chomp.to_i
+      @player2_played.push(@player2_input)
+
+      @arr[@player2_input - 1] = " [ O ] "
+      puts @arr.each_slice(3) { |x| puts x.join puts}
+      end
       # Make sure user has the correct input
 
 =begin
