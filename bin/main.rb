abd8af99cdfe58dfaa1068f4b462e052abffb1f3
yirano <tiffany.arellano@outlook.com>
2019-10-07 16:57:09 -0400

    Implement true/false game continuation
    Mock whether the game should end based on a true/false implementation

--- bin/main.rb
+++ bin/main.rb
@@ -5,31 +5,55 @@ puts "Player 1, What is your name?"
 puts "Now, Player 2, What is your name?"
 @player2 = gets.chomp
 puts "Let's play! #{@player1} #{@player2}"
-count = 0
+continue_game = true
 
 # While nobody has won
-while count < 3
-
+while continue_game
   # get input from player 1
   puts "Choose a number between 1-9 #{@player1}"
   @player1_input = gets.chomp.to_i
-  
   # Make sure user has the correct input
   if @player1_input.is_a?(Integer) && @player1_input > 0 && @player1_input <= 9
-    puts "#{@player2} now its your turn, choose a number between 1-9"
-    @player2_input = gets.chomp
-    if @player2_input.is_a?(Integer) && @player2_input > 0 && @player2_input <= 9
-      puts "#{@player1} now its your turn, choose a number between 1-9"
-    else
-      # If player 2 has wrong input
-      puts "FOLLOW THE RULES"
+    print "Player one, you have input #{@player1_input}"
+    puts
+    puts "Have you won? press 'y' for yes and 'n' for no 't' for tie"
+    ans = gets.chomp
+    if ans == 'y'
+      continue_game = false
+      break
+    elsif ans == 't'
+      puts 'IT IS A TIE'
+      break
     end
   else
       # If player 1 has wrong input
     puts "YOU GOTTA PUT IN SOMETHING RIGHT"
   end
-  count +=1
+
+  puts "HERE IS YOUR BOARD RIGHT HERE."
+
+  puts "Choose a number between 1-9 #{@player2}"
+  @player2_input = gets.chomp.to_i
+  if @player2_input.is_a?(Integer) && @player2_input > 0 && @player2_input <= 9
+    puts "#{@player2} now its your turn, choose a number between 1-9"
+    puts
+    puts "Have you won? press 'y' for yes and 'n' for no 't' for tie"
+    ans = gets.chomp
+    if ans == 'y'
+      continue_game = false
+      break
+    elsif ans == 't'
+      puts 'IT IS A TIE'
+      break
+    end
+  else
+    # If player 2 has wrong input
+    puts "FOLLOW THE RULES"
+  end
+
+  puts "HERE IS YOUR BOARD RIGHT HERE."
+  
 end
 
-# Congratulate winner
-puts "YOU WON #{@player1}!!!"
+
+
