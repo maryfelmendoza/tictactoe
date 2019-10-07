afbfda539c0c22317d47ad1822d810d4120fcdfd
yirano <tiffany.arellano@outlook.com>
2019-10-07 12:47:12 -0400

    Organize class files
    Transfer some codes from the main.rb into game.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,16 @@
-def create
-  # >>> This creates the board and marks every grid with a number 1 - 9
-  for i in 1..9
-    @arr.push(" [ #{i} ] ").each_slice(3)
+class Board
+  def initalize
+    # self.create
   end
-  @arr.each_slice(3) { |x| puts x.join puts}
-  @arr
+  
+  def self.create
+    @arr = []
+    # >>> This creates the board and marks every grid with a number 1 - 9
+    for i in 1..9
+      @arr.push(" [ #{i} ] ").each_slice(3)
+    end
+    @arr.each_slice(3) { |x| puts x.join puts}
+    @arr
+  end
+
 end
