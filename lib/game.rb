df2a3c0b99a24d954091de3c0c6c80516534bc4b
yirano <tiffany.arellano@outlook.com>
2019-10-07 17:39:12 -0400

    Add what happens when game is tied

--- lib/game.rb
+++ lib/game.rb
@@ -8,6 +8,7 @@ class Game
     @board_array = []
     @box_played = []
     @current_player = 1
+    @winner
     @winning_combos= [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
   end
   
@@ -25,62 +26,52 @@ class Game
     self.board
     while ct < 9
       if @current_player == 1
-        puts "Choose a number between 1-9 #{@player1}"
-        @player1_input = gets.chomp.to_i
-        if @box_played.include? @player1_input
-          puts "PICK SOMETHING ELSE YOU CHEATER"
-        else
-          @player1_played.push(@player1_input)
-          @box_played.push(@player1_input)
-          @board_array[@player1_input - 1] = " [ X ] "
-          puts @board_array.each_slice(3) { |x| puts x.join puts}
-          @current_player = 2
-          self.game_over?
-        end
+        player_input = @player1_input
+        player_played = @player1_played
+        player = @player1
+        sym = 'X'
+        @current_player = 2
+        self.run(player_input, player_played, player, sym)
       elsif @current_player == 2
-        puts "Choose a number between 1-9 #{@player2}"
-        @player2_input = gets.chomp.to_i
-        if @box_played.include? @player2_input
-          puts "PICK SOMETHING ELSE YOU CHEATER"
-        else
-          @player2_played.push(@player2_input)
-          @box_played.push(@player2_input)
-          @board_array[@player2_input - 1] = " [ O ] "
-          puts @board_array.each_slice(3) { |x| puts x.join puts}
-          @current_player = 1
-          self.game_over?
-        end
+        player_input = @player2_input
+        player_played = @player2_played
+        player = @player2
+        sym = 'O'
+        @current_player = 1
+        self.run(player_input, player_played, player, sym)
       end
       ct += 1
     end
   end
 
-  def test
-    if @current_player == 1
-      puts "Choose a number between 1-9 #{@player1}"
-      @player1_input = gets.chomp.to_i
-      if @box_played.include? @player1_input
+  def run(player_input, player_played, player, sym)
+      puts "Choose a number between 1-9 #{player}"
+      player_input = gets.chomp.to_i
+      if @box_played.include? player_input
         puts "PICK SOMETHING ELSE YOU CHEATER"
       else
-        @player1_played.push(@player1_input)
-        @box_played.push(@player1_input)
-        @board_array[@player1_input - 1] = " [ X ] "
+        player_played.push(player_input)
+        @box_played.push(player_input)
+        @board_array[player_input - 1] = " [ #{sym} ] "
         puts @board_array.each_slice(3) { |x| puts x.join puts}
-        @current_player = 2
         self.game_over?
       end
     end
-  end
 
   def game_over?
     for i in 0...@winning_combos.length
       if (@winning_combos[i] - @player1_played).empty?
         puts "YOU WON PLAYER ONE!"
+        @winner = @player1
         exit
       elsif (@winning_combos[i] - @player2_played).empty?
         puts "YOU WON PLAYER TWO!"
+        @winner = @player2
         exit
       end
+    end
+    if @winner == nil 
+      puts "YOU'RE BOTH LOSERS!"
     end
   end
 end
