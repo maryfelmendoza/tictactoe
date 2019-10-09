650b0a88495a520049130e8d1a832fee0d922107
yirano <tiffany.arellano@outlook.com>
2019-10-09 11:47:58 -0400

    Set up game class Created the following methods: - play - check - game_over

--- bin/main.rb
+++ bin/main.rb
@@ -1,15 +1,58 @@
 require_relative  '../lib/game.rb'
 
-# Set up game
-puts "Welcome!"
-puts
-print "Player 1, What is your name?: "
-player1 = gets.chomp
-print "Now, Player 2, What is your name?: "
-player2 = gets.chomp
-puts
-puts "Let's play! #{player1} #{player2}"
-puts
-game = Game.new(player1, player2)
+# puts "Welcome!"
+# puts
+# print "Player 1, What is your name?: "
+# player1 = gets.chomp
+# print "Now, Player 2, What is your name?: "
+# player2 = gets.chomp
+# puts
+# puts "Let's play! #{player1} #{player2}"
+# puts
 
-game.play
+player1 = "Apple"
+player2 = "Banana"
+
+game = Game.new(player1, player2)
+ct = 0
+while ct < 11
+puts "Pick a box to play #{player1}!"
+box = gets.chomp.to_i
+game.play(player1, box)
+
+puts "It's your turn to pick #{player2}"
+box = gets.chomp.to_i
+game.play(player2, box)
+ct += 1
+end
+
+
+
+
+=begin
+
+
+1. welcome player
+2. get player names
+3. start the game
+
+>>>>> GAME CLASS <<<<<
+- call game class
+
+  >>> def play <<<
+******** LOOP START
+ 4. let player 1 input
+  - check if player won
+  - display updated board
+ 5. switch to player 2
+  - check if player won
+  - display updated board
+******** LOOP END
+
+ >>> def game_over <<<
+ 6. if game over
+  - Win?
+  - Tie?
+  - Start again?  
+=end
+
--- lib/game.rb
+++ lib/game.rb
@@ -10,74 +10,46 @@ class Game
     @box_played = []
     @current_player = 1
     @winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    @ct = 0
+    @count = 0
   end
 
   def board
-    # >>> This creates the board and marks every grid with a number 1 - 9
-    # for i in 1..9
-    #   @board_array.push(" [ #{i} ] ").each_slice(3)
-    # end
     (1..9).each do |i|
       @board_array.push(" [ #{i} ] ").each_slice(3)
     end
     @board_array.each_slice(3) { |x| puts x.join puts }
   end
 
-  def play
-    # While nobody has won
+  def play(player, box)
     board
-    while @ct < 9
-      if @current_player == 1
-        puts "Choose a number between 1-9 #{@player1}"
-        @player1_input = gets.chomp.to_i
-        player_input = @player1_input
-        player_played = @player1_played
-        player = @player1
-        sym = 'X'
-        @current_player = 2
-        run(player_input, player_played, player, sym)
-      elsif @current_player == 2
-        puts "Choose a number between 1-9 #{@player2}"
-        @player2_input = gets.chomp.to_i
-        player_input = @player2_input
-        player_played = @player2_played
-        player = @player2
-        sym = 'O'
-        @current_player = 1
-        run(player_input, player_played, player, sym)
-      end
-    end
-    @ct += 1
-  end
-
-  def run(player_input, player_played, _player, sym)
-    if (@box_played.include? player_input) && !player_input.nil?
-      puts 'PICK SOMETHING ELSE YOU CHEATER'
+    if player == @player1
+      player_played = @player1_played
     else
-      player_played.push(player_input)
-      @box_played.push(player_input)
-      @board_array[player_input - 1] = " [ #{sym} ] "
-      puts @board_array.each_slice(3) { |x| puts x.join puts }
-      game_over?
+      @player2_played.push(box)
+      player_played = @player2_played
     end
+    check(player, player_played)
   end
 
-  def game_over?
+  def check(player, player_played)
+    puts "Sup #{player}, so far you got #{player_played}"
+
+    player_played.push(box)
+    game_over?(player, player_played)
+  end
+
+  def game_over?(player, player_played)
     (0...@winning_combos.length).each do |i|
-      if (@winning_combos[i] - @player1_played).empty?
-        puts 'YOU WON PLAYER ONE!'
-        @winner = @player1
+      if (@winning_combos[i] - player_played).empty?
+        puts "YOU WON #{player.upcase}!!"
         exit
-      elsif (@winning_combos[i] - @player2_played).empty?
-        puts 'YOU WON PLAYER TWO!'
-        @winner = @player2
-        exit
+      else 
+        puts 'KEEP PLAYING!'
       end
     end
-    if @box_played.length == 9 && @winner.nil?
-      puts "YOU'RE BOTH LOSERS"
-      exit
-    end
   end
+    # if @box_played.length == 9 && @winner.nil?
+    #   puts "YOU'RE BOTH LOSERS"
+    #   exit
+    # end
 end
