6697908b75b2e286c88435f8c7d3079e3668f51a
yirano <tiffany.arellano@outlook.com>
2019-10-11 12:41:58 -0400

    Start DRY code on main Start the beginning of looping through the game play on main

--- bin/main.rb
+++ bin/main.rb
@@ -16,45 +16,33 @@ player1 = name("Player 1")
 player2 = name("Player 2")
 
 game = Game.new(player1, player2)
-loop do
-  loop do
-    puts
-    print "Pick a box to play #{player1}! > "
-    box = gets.chomp.to_i
-    if !box.between?(1, 9)
-      puts
-      print "Please input a number between 1 and 9"
-      puts
-    else
-      if game.cheater?(box) == false
-        game.play(player1, box, 1)
-        break
-      else
-        puts "That has been played already!"
-      end
-    end
-  end
-  break if game.game_over[0] == true
 
+
+
+def turns
   loop do
-    puts
-    print "It's your turn to pick #{player2} > "
-    box = gets.chomp.to_i
-    if !box.between?(1, 9)
+    loop do
       puts
-      print "Please input a number between 1 and 9"
-      puts
-    else
-      if game.cheater?(box) == false
-        game.play(player2, box, 2)
-        break
+      print "Pick a box to play #{player1}! > "
+      box = gets.chomp.to_i
+      if !box.between?(1, 9)
+        puts
+        print "Please input a number between 1 and 9"
+        puts
       else
-        puts "That has been played already!"
+        if game.cheater?(box) == false
+          game.play(player1, box, 1)
+          break
+        else
+          puts "That has been played already!"
+        end
       end
     end
+    break if game.game_over[0] == true
   end
-  break if game.game_over[0] == true
 end
+
+
 
 if game.game_over[0] == true
   puts "GAME OVER #{game.game_over[1]}!"
