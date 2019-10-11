e908ccc65bcb01ae23d8b15e2c6af3b551543555
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-11 12:04:14 -0500

    Method inside the main

--- bin/main.rb
+++ bin/main.rb
@@ -12,36 +12,45 @@ def name(player)
   player
 end
 
+current_player = 1
 player1 = name("Player 1")
 player2 = name("Player 2")
-
-game = Game.new(player1, player2)
-
+$game = Game.new(player1, player2)
 
 
-def turns
+
+def turns(player, n)
+
   loop do
     loop do
       puts
-      print "Pick a box to play #{player1}! > "
+      print "Pick a box to play #{player}! > "
       box = gets.chomp.to_i
       if !box.between?(1, 9)
         puts
         print "Please input a number between 1 and 9"
         puts
       else
-        if game.cheater?(box) == false
-          game.play(player1, box, 1)
+        if $game.cheater?(box) == false
+          $game.play(player, box, n)
           break
         else
           puts "That has been played already!"
         end
       end
     end
-    break if game.game_over[0] == true
+    break if $game.game_over[0] == true
   end
 end
 
+if current_player == 1
+  turns(player1, 1)
+  current_player = 2
+else
+  current_player == 2
+  turns(player2, 2)
+  current_player == 1
+end
 
 
 if game.game_over[0] == true
