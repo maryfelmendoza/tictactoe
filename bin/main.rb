873a0bfead645c40cdf505f3b171ab65c4efa504
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-16 22:47:57 -0500

    welcome info

---
+++ bin/main.rb
@@ -0,0 +1,24 @@
+require '../lib/game.rb'
+require '../lib/player.rb'
+require '../lib/board.rb'
+
+# welcome info
+
+puts
+puts "*" * 27
+puts "  WELCOME TO TIC-TAC-TOE!"
+puts "*" * 27
+puts
+
+puts 'What is your name playe 1?'
+name1 = gets.chomp
+
+puts 'What is your name player 2?'
+name2 = gets.chomp
+
+
+#game
+
+time = 0
+while < 9
+end
