6e9d742b5bd7a4bbc013d4f9fe4465e521bd1e13
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-17 01:40:27 -0500

    space fixed

--- lib/game.rb
+++ lib/game.rb
@@ -47,7 +47,7 @@ class Game
     end
     win_diagonals(board).each do |win_diagonal|
       next unless win_diagonal.all?(X_SYM) || win_diagonal.all?(O_SYM)
-      
+
       winner = win_diagonal
     end
     winner
